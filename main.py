b913498aa46b16ef59a4606953a15d3f63f60b9c
Adrian <146006794+adrian4096@users.noreply.github.com>
2026-01-17 01:22:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,7 @@ app = FastAPI(title="FRITIME Boolean Service")
 app.include_router(api.router, prefix="/bool")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:8080",
-        "http://127.0.0.1:8080",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:8002",
-        "http://127.0.0.1:8002",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
